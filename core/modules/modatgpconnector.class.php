e888d6ec76ef226b665e3a8ec9e65d59ab87aad4
Florian HENRY <florian.henry@open-concept.pro>
2022-12-13 15:55:11 +0100

    review metyhosd

--- core/modules/modatgpconnector.class.php
+++ core/modules/modatgpconnector.class.php
@@ -46,7 +46,7 @@ class modatgpconnector extends DolibarrModules
 
 		$this->editor_name = 'ATM Consulting';
 		$this->editor_url = 'https://www.atm-consulting.fr';
-		
+
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
 		$this->numero = 104068; // 104000 to 104999 for ATM CONSULTING
@@ -70,7 +70,7 @@ class modatgpconnector extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		$this->picto='atgpconnector@atgpconnector';
-		
+
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /atgpconnector/core/xxxxx) (0=disable, 1=enable)
 		// for specific path of parts (eg: /atgpconnector/core/modules/barcode)
@@ -205,7 +205,7 @@ class modatgpconnector extends DolibarrModules
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
-		
+
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'atgpconnector_write';	// Permission label
 		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
@@ -249,9 +249,9 @@ class modatgpconnector extends DolibarrModules
 		//							'target'=>'',
 		//							'user'=>2);				                // 0=Menu for internal users, 1=external users, 2=both
 		// $r++;
-		
+
 /*
-		$this->menu[$r]=array(	
+		$this->menu[$r]=array(
 			'fk_menu'=>0,			                // Put 0 if this is a top menu
 			'type'=>'top',			                // This is a Top menu entry
 			'titre'=>$langs->trans('TopMenuatgpconnector'),
@@ -282,7 +282,7 @@ class modatgpconnector extends DolibarrModules
 			'user'=>2
 		);
 		$r++;
-		
+
 		$this->menu[$r]=array(
 			'fk_menu'=>'fk_mainmenu=atgpconnector,fk_leftmenu=atgpconnector_left',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
 			'type'=>'left',			                // This is a Left menu entry
@@ -298,7 +298,7 @@ class modatgpconnector extends DolibarrModules
 			'user'=>2
 		);				                // 0=Menu for internal users, 1=external users, 2=both
 		$r++;
-		
+
 
 		$this->menu[$r]=array(
 			'fk_menu'=>'fk_mainmenu=atgpconnector,fk_leftmenu=atgpconnector_left',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
@@ -316,7 +316,7 @@ class modatgpconnector extends DolibarrModules
 		);				                // 0=Menu for internal users, 1=external users, 2=both
 		$r++;
 */
-		
+
 		// Exports
 		$r=1;
 
@@ -393,7 +393,7 @@ class modatgpconnector extends DolibarrModules
 	function init($options='')
 	{
 		$sql = array();
-		
+
 		define('INC_FROM_DOLIBARR',true);
 
 		// require dol_buildpath('/atgpconnector/script/create-maj-base.php');
@@ -485,7 +485,7 @@ class modatgpconnector extends DolibarrModules
 					$TFilter = array();
 			}
 
-			$cronjobs->fetch_all('DESC', 't.rowid', 0, 0, -1, $TFilter);
+			$cronjobs->fetchAll('DESC', 't.rowid', 0, 0, -1, $TFilter);
 
 			if(empty($cronjobs->lines))
 			{
@@ -563,7 +563,7 @@ class modatgpconnector extends DolibarrModules
 					$TFilter = array();
 			}
 
-			$cronjobs->fetch_all('DESC', 't.rowid', 0, 0, -1, $TFilter);
+			$cronjobs->fetchAll('DESC', 't.rowid', 0, 0, -1, $TFilter);
 
 			if(! empty($cronjobs->lines))
 			{
